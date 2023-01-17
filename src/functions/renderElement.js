389e39a6e6586f351d2fc5d3ac0196b2accabd2f
GoorezyEST <goorezyestudios@gmail.com>
2023-01-17 19:30:43 -0300

    Added subtitles

--- src/functions/renderElement.js
+++ src/functions/renderElement.js
@@ -1,10 +1,14 @@
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import js_beautify from "js-beautify";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const renderElement = (item) => {
   if (item.hasOwnProperty("text")) {
-    return <p>{item.text}</p>;
+    return (
+      <div className="text_container">
+        <h3>{item.text.title}</h3>
+        <p>{item.text.text}</p>
+      </div>
+    );
   } else if (item.hasOwnProperty("link")) {
     return (
       <a href={item.link} target="_blank" rel="noopener noreferrer">
@@ -14,14 +18,13 @@ const renderElement = (item) => {
   } else if (item.hasOwnProperty("img")) {
     return <img src={item.img.src} alt={item.img.alt} />;
   } else if (item.hasOwnProperty("code")) {
-    const formattedCode = js_beautify.html(item.code.code);
     return (
       <SyntaxHighlighter
         className="codeBlock"
         language={item.code.language}
         style={atomDark}
       >
-        {formattedCode}
+        {item.code.code}
       </SyntaxHighlighter>
     );
   }
